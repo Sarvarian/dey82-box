7910190f6a85eb4091c0e4ee23735e4f292a5d8c
Sarvarian <Sarvarian@Hotmail.com>
2024-02-05 17:00:05 +0330

    A Simple SDL Application

--- primes.hpp
+++ primes.hpp
@@ -4,20 +4,21 @@ namespace ab
 { namespace primitives
 {
 
-#define i8   Sint8
-#define i16  Sint16
-#define i32  Sint32
-#define i64  Sint64
-#define u8   Uint8
-#define u16  Uint16
-#define u32  Uint32
-#define u64  Uint64
+#define i8    Sint8
+#define i16   Sint16
+#define i32   Sint32
+#define i64   Sint64
+#define u8    Uint8
+#define u16   Uint16
+#define u32   Uint32
+#define u64   Uint64
 
 #define ptr   void*
-#define null (void*)0
+#define null  NULL
 
 #define and   &&
 #define or    ||
+#define not   !
 
 #define sub
 #define proc  void
